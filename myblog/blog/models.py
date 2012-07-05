29a1074adbd4dd0296a9663a5ee1b2f4eff6e5b8
AITI Student <aiti-student@AITI-87.(none)>
2012-07-05 15:25:58 +0000

    blog

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -5,16 +5,16 @@ class Post(models.Model):
   body=models.TextField()
   created=models.DateField()
   updated=models.DateField()
-  def __unicode__():
-    return title
+  def __unicode__(self):
+    return self.title
 class Comment(models.Model):
   body=models.TextField()
   author=models.CharField(max_length=60)
   created=models.DateField()
   updated=models.DateField()
   post=models.ForeignKey(Post)
-  def __unicode__():
-    return author
+  def __unicode__(self):
+    return self.author
 admin.site.register(Post)
 admin.site.register(Comment)
 
