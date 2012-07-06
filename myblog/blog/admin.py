41fbf62c48e4afc9bcfd21685df324bd2bdaa95d
AITI Student <aiti-student@AITI-87.(none)>
2012-07-06 12:56:27 +0000

    blog project

---
+++ myblog/blog/admin.py
@@ -0,0 +1,17 @@
+from  blog.models import Comment,Post
+from django.contrib import admin
+
+class PostAdmin(admin.ModelAdmin):
+  list_display=('title','created','updated')
+  list_filter = ('title','created')
+  search_fields = ('title', 'body')
+
+class CommentAdmin(admin.ModelAdmin):
+  list_display=('post','author','body','created','updated')
+  list_filter = ('author','created')
+class CommentInline(admin.TabularInline):
+  model='Comment'
+admin.site.register(Post,PostAdmin)
+admin.site.register(Comment,CommentAdmin)
+#admin.site.register(Post)
+#admin.site.register(Comment)
--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.db import models
-def author_60(self):
-  return self.author[:60]
 
 class Post(models.Model):
   title=models.CharField(max_length=100)
@@ -10,11 +8,6 @@ class Post(models.Model):
   updated=models.DateField()
   def __unicode__(self):
     return self.title
-
-class PostAdmin(admin.ModelAdmin):
-  list_display=('title','created','update')
-  list_filter = ('title','created')
-  search_fields = ('title', 'body')
 
 class Comment(models.Model):
   body=models.TextField()
@@ -25,14 +18,7 @@ class Comment(models.Model):
   def __unicode__(self):
     return self.author
 
-class CommentAdmin(admin.ModelAdmin):
-  list_display=('post','author_60','body','created','update')
-  list_filter = ('author','created')
 
-class CommentInline(admin.TabularInline):
-  model='comment'
-admin.site.register(Post,PostAdmin)
-admin.site.register(Comment,CommentAdmin)
 
 
 
